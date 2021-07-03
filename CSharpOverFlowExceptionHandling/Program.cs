295219b036aa2cdae21371dd514d5c4840286136
markandmark1111@gmail.com <markandmark1111@gmail.com>
2021-07-03 03:15:58 +0200

    Add project files.

---
+++ CSharpOverFlowExceptionHandling/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CSharpOverFlowExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 65445789;
+            int b = 7896;
+
+            checked
+            {
+                try
+                {
+                    while (true)
+                    {
+                        a = a * b;
+                        Console.WriteLine(a);
+                    }
+                }
+                catch (OverflowException e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            }// end checked
+        }
+    }
+}
